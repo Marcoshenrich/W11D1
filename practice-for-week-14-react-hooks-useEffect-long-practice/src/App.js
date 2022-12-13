a698322d6aa780049dec55966f196dc7bd48813d
Marcos <jmarcoshenrich@gmail.com>
2022-12-12 17:02:02 -0800

    turkeys for days

--- practice-for-week-14-react-hooks-useEffect-long-practice/src/App.js
+++ practice-for-week-14-react-hooks-useEffect-long-practice/src/App.js
@@ -1,16 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Message from "./components/Message";
 import PictureDisplay from "./components/PictureDisplay";
 
 function App() {
   const [size, setSize] = useState('s');
   const [featherCount, setFeatherCount] = useState(0);
-  const [featherColors, setFeatherColors] = useState([]);
   const [isRed, setIsRed] = useState(false);
   const [isOrange, setIsOrange] = useState(false);
   const [isBrown, setIsBrown] = useState(false);
   const [isLightBrown, setIsLightBrown] = useState(false);
   const [isYellow, setIsYellow] = useState(false);
+  const [featherColors, setFeatherColors] = useState([]);
+
+  console.log(featherColors)
+
+  useEffect(()=>{
+    const colors = [];
+    if (isRed) colors.push('red');
+    if (isOrange) colors.push('orange');
+    if (isBrown) colors.push('brown');
+    if (isLightBrown) colors.push('light-brown');
+    if (isYellow) colors.push('yellow');
+    setFeatherColors(colors);
+  }, [isRed, isOrange, isBrown, isLightBrown, isYellow])  
+
+
 
   return (
     <>
@@ -52,12 +66,13 @@ function App() {
         <label><input
           type="checkbox"
           onChange={(e) => setIsLightBrown(e.currentTarget.checked)}
-        />Light Brown</label>
+          />Light Brown</label>
         <label><input
           type="checkbox"
-          onChange={(e) => setIsYellow(e.currentTarget.checked)}
-        />Golden Yellow</label>
+          onChange={(e) => setIsYellow(e.currentTarget.checked)} 
+          />Golden Yellow</label>
       </div>
+          
 
       {/* Generated display based on user selections above */}
       <h3 className="button-controls">Enjoy your turkey</h3>
